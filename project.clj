5a4c606398d72666e9ad20c55dcad63359d2c94a
Thiago Fernandes Massa <thiagown@gmail.com>
2016-03-17 18:10:14 +0100

    Added cprop package for config management

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.paypal.sdk/rest-api "1.4.1"]])
+                 [cprop "0.1.6"]
+                 [com.paypal.sdk/rest-api-sdk "1.4.1"]])
