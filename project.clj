e95a26931115aa93b729b796325796b677f06680
Thiago Fernandes Massa <thiagown@gmail.com>
2016-03-17 16:10:06 +0100

    Added paypal sdk to the dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.paypal.sdk/rest-api "1.4.1"]])
